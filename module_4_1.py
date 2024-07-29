4889b164038595f87f29e9c13ee4ba208aa52f64
fedka1979 <fedka-3@yandex.ru>
2024-07-29 11:59:15 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import devide as fa
+from true_math import devide as tr
+
+result1 = fa(69, 3)
+result2 = fa(3, 0)
+result3 = tr(49, 7)
+result4 = tr(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
